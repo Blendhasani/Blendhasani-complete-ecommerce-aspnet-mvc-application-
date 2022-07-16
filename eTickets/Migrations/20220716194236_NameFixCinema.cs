4738b2e07e9fef29829a9ada85bd9886197bdd21
Blendhasani <bh51257@ubt-uni.net>
2022-07-16 23:15:21 +0200

    AppDbInitializier

---
+++ eTickets/Migrations/20220716194236_NameFixCinema.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class NameFixCinema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "logo",
+                table: "Cinemas",
+                newName: "Logo");
+
+            migrationBuilder.RenameColumn(
+                name: "Decription",
+                table: "Cinemas",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Logo",
+                table: "Cinemas",
+                newName: "logo");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Cinemas",
+                newName: "Decription");
+        }
+    }
+}
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -8,10 +8,10 @@ namespace eTickets.Models
 
         [Key]
         public int Id { get; set; }
-        public string logo { get; set; }
+        public string Logo { get; set; }
         public string Name { get; set; }
 
-        public string Decription { get; set; }
+        public string Description { get; set; }
 
 
         public List<Movie> Movies { get; set; }
