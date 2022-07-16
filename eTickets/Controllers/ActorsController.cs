828c01daa3b9c16efe72e8f184ae920bb8c9fd08
Blendhasani <bh51257@ubt-uni.net>
2022-07-16 23:36:29 +0200

    Kontrollerat

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        // kur don mu thirr psh / ActorsController kjo vjen nga 
+        //IActionResult Index
+
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
