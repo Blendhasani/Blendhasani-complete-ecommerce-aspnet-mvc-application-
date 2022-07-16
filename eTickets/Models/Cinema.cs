4a492d4fe80779cdd81af35ea985f1460d9ef250
Blendhasani <bh51257@ubt-uni.net>
2022-07-16 20:37:26 +0200

    Modelet dhe AppDbContext

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string logo { get; set; }
+        public string Name { get; set; }
+
+        public string Decription { get; set; }
+
+
+        public List<Movie> Movies { get; set; }
+    }
+}
